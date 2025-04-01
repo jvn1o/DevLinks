05d6c3fd7c7d8c904dccc38c950b80a9d0d06599
jvn1o <junok0987@naver.com>
2025-04-01 22:40:41 +0900

    Header icon 수정

--- frontend/devlinks/components/DefaultHeader.vue
+++ frontend/devlinks/components/DefaultHeader.vue
@@ -6,23 +6,29 @@ const tabs = ["Algorithm & Data Structures", "API & Documentation", "Cloud & Dev
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="w-137">
       <!-- 우측 아이콘 (북마크, 프로필) -->
-      <div class="d-flex align-items-center">
-        <NuxtLink to="/login">
-          <img src="/assets/image/icon/profile.svg" alt="Profile" width="32" height="32" class="rounded-circle"/>
-        </NuxtLink>
-        <NuxtLink to="/mypage/bookmark" class="me-3">
-          <img src="/assets/image/icon/bookmark.svg" alt="Bookmark" width="24" height="24"/>
-        </NuxtLink>
-        <NuxtLink to="/mypage/bookmark" class="me-3">
-          <img src="/assets/image/icon/bookmark.svg" alt="Bookmark" width="24" height="24"/>
-        </NuxtLink>
+      <div class="d-flex align-items:center p-2">
+        <span class="w-32 h-32 p-1">
+          <NuxtLink to="/login">
+            <img src="/assets/image/icon/profile.svg" alt="Profile" width="32" height="32" class="rounded-circle"/>
+          </NuxtLink>
+        </span>
+        <span class="w-25 h-25 p-1">
+          <NuxtLink to="/mypage/bookmark" class="me-3">
+            <img src="/assets/image/icon/bookmark.svg" alt="Bookmark" width="24" height="24"/>
+          </NuxtLink>
+        </span>
+        <span class="w-25 h-25 p-1">
+          <NuxtLink to="/mypage/alarm" class="me-3">
+            <img src="/assets/image/icon/alarm.svg" alt="Alarm" width="24" height="24"/>
+          </NuxtLink>
+        </span>
       </div>
     </div>
 
     <div class="container">
       <!-- 로고 (홈 이동) -->
       <NuxtLink to="/" class="navbar-brand">
-        <img src="/assets/image/DevLinks.svg" alt="Logo" width="40" height="40"/>
+        <img src="/assets/image/DevLinks.svg" alt="Logo" width="32" height="32"/>
       </NuxtLink>
 
       <!-- 네비게이션 메뉴 -->
