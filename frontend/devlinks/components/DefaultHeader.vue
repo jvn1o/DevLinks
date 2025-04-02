cb699a9fc876010dc7119d1e2aa227576dd1afa0
jvn1o <junok0987@naver.com>
2025-04-02 20:11:09 +0900

    Header 부분 수정

--- frontend/devlinks/components/DefaultHeader.vue
+++ frontend/devlinks/components/DefaultHeader.vue
@@ -3,27 +3,27 @@ const tabs = ["Algorithm & Data Structures", "API & Documentation", "Cloud & Dev
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light p-0">
     <!-- 우측 아이콘 (북마크, 프로필) -->
-    <div class="d-flex align-items:center p-2">
-      <span class="w-32 h-32 p-1">
+    <div class="d-flex justify-content-center align-items-center p-2">
+      <div class="w-32 h-32 p-1">
         <NuxtLink to="/login">
           <img src="/assets/image/icon/profile.svg" alt="Profile" width="32" height="32" class="rounded-circle"/>
         </NuxtLink>
-      </span>
-      <span class="w-25 h-25 p-1">
+      </div>
+      <div class="w-25 h-25 p-1">
         <NuxtLink to="/mypage/bookmark" class="me-3">
           <img src="/assets/image/icon/bookmark.svg" alt="Bookmark" width="24" height="24"/>
         </NuxtLink>
-      </span>
-      <span class="w-25 h-25 p-1">
+      </div>
+      <div class="w-25 h-25 p-1">
         <NuxtLink to="/mypage/alarm" class="me-3">
           <img src="/assets/image/icon/alarm.svg" alt="Alarm" width="24" height="24"/>
         </NuxtLink>
-      </span>
+      </div>
     </div>
 
-    <div class="container">
+    <div class="container p-0">
       <!-- 로고 (홈 이동) -->
       <NuxtLink to="/" class="navbar-brand">
         <img src="/assets/image/DevLinks.svg" alt="Logo" width="32" height="32"/>
@@ -31,9 +31,9 @@ const tabs = ["Algorithm & Data Structures", "API & Documentation", "Cloud & Dev
 
       <!-- 네비게이션 메뉴 -->
       <div class="collapse navbar-collapse">
-        <ul class="navbar-nav mx-auto">
+        <ul class="navbar-nav">
           <li v-for="(tab, index) in tabs" :key="index" class="nav-item">
-            <NuxtLink :to="`/${tab.toLowerCase().replace(/ /g, '-')}`" class="nav-link fw-normal">
+            <NuxtLink :to="`/${tab.toLowerCase().replace(/ /g, '-')}`" class="nav-link fw-semibold">
               {{ tab }}
             </NuxtLink>
           </li>
