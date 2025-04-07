46faec2cc8c1843dc3cf258fdec4610b5789b40e
jvn1o <junok0987@naver.com>
2025-04-07 23:51:44 +0900

    회원가입 성공 안내 화면

---
+++ frontend/devlinks/pages/signup-success.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="container d-flex flex-column align-items-center justify-content-center text-center vh-70">
+    <img src="@/assets/image/icon/checked.png" alt="Success Icon" class="mb-4" style="width: 100px; height: 100px;" />
+    <div class="fs-2 fw-bold text-dark mb-4">
+      Your account was successfully created!
+    </div>
+    <RouterLink to="/signin" class="btn btn-primary px-4 py-2">
+      Back to Login
+    </RouterLink>
+  </main>
+</template>
+
+<style scoped>
+/* Bootstrap에는 vh-70 클래스가 없으므로 직접 정의 */
+.vh-70 {
+  height: 70vh;
+}
+</style>
