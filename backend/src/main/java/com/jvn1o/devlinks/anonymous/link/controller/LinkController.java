fbf589c7a220f9a0f654a58865cf8b8ed1295acf
jvn1o <junok0987@naver.com>
2025-04-18 23:17:26 +0900

    RequestMapping 주소 변경

--- backend/src/main/java/com/jvn1o/devlinks/anonymous/link/controller/LinkController.java
+++ backend/src/main/java/com/jvn1o/devlinks/anonymous/link/controller/LinkController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/anonymous/links")
+@RequestMapping("/links")
 @RequiredArgsConstructor
 public class LinkController {
 
