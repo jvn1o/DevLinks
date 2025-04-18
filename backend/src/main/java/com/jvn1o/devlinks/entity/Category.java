fdaddf9614812a042d5262573e3f7db3e3cca8a6
jvn1o <junok0987@naver.com>
2025-04-18 23:39:49 +0900

    Category Entity 생성

---
+++ backend/src/main/java/com/jvn1o/devlinks/entity/Category.java
@@ -0,0 +1,24 @@
+package com.jvn1o.devlinks.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "category")
+public class Category {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+}
