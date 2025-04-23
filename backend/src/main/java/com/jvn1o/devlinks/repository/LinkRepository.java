1bd93cc2840f5c54fa04686cf583d716c2d1398f
jvn1o <junok0987@naver.com>
2025-04-23 22:12:07 +0900

    link repository jpa 연결

--- backend/src/main/java/com/jvn1o/devlinks/repository/LinkRepository.java
+++ backend/src/main/java/com/jvn1o/devlinks/repository/LinkRepository.java
@@ -1,9 +1,14 @@
 package com.jvn1o.devlinks.repository;
 
+import com.jvn1o.devlinks.entity.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 
 
 @Repository
-public class LinkRepository {
+public interface LinkRepository extends JpaRepository<Link, Long> {
 
 }
