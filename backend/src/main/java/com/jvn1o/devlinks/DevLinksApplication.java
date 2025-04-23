07c3e58881be12694e98d18c3b29b28c4c84438f
jvn1o <junok0987@naver.com>
2025-04-23 22:33:43 +0900

    class 수정

---
+++ backend/src/main/java/com/jvn1o/devlinks/DevLinksApplication.java
@@ -0,0 +1,13 @@
+package com.jvn1o.devlinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevLinksApplication.class, args);
+	}
+
+}
