9288f5c3b2ca47aa6de0cffd0f3aed49f768fb48
jvn1o <junok0987@naver.com>
2025-04-26 23:53:49 +0900

    gradle 설정

--- backend/src/main/java/com/jvn1o/devlinks/entity/Member.java
+++ backend/src/main/java/com/jvn1o/devlinks/entity/Member.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
 import java.util.List;
