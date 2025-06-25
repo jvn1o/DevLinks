4ed068885798b6fb8d232fdc8b16e53027c6b725
jvn1o <junok0987@naver.com>
2025-06-25 19:12:38 +0900

    Auth 인증 방식을 경로에 따라 검증하도록 변경 및 toggleSidebar 동작은 Sidebar.vue에서만 동작하게끔 분리 작업

--- frontend/devlinks/composables/useAuthRedirect.ts
+++ frontend/devlinks/composables/useAuthRedirect.ts
@@ -4,9 +4,17 @@ export const useAuthRedirect = (
     toggleSidebar?: () => void
 ) => {
     const router = useRouter()
+    const config = useRuntimeConfig()
 
     const navigateWithAuth = async (targetPath: string) => {
         console.log('[navigateWithAuth] called →', targetPath)
+
+        // nuxt.config.ts 설정으로 강제 우회 (개발용)
+        if (config.public.DEV_BYPASS_AUTH === 'true') {
+            console.log('[navigateWithAuth] 개발 모드 인증 우회 중 →', targetPath)
+            router.push(targetPath)
+            return
+        }
         
         if (isAnonymous()) {
             console.log('[navigateWithAuth] user is anonymous → showing modal...')
@@ -24,8 +32,6 @@ export const useAuthRedirect = (
 
         console.log('[navigateWithAuth] user is authenticated → navigating to:', targetPath)
         router.push(targetPath)
-
-        if (toggleSidebar) toggleSidebar()
     }
 
     return { navigateWithAuth }
--- frontend/devlinks/middleware/auth.ts
+++ frontend/devlinks/middleware/auth.ts
@@ -1,18 +1,26 @@
-// // 인증이 필요한 페이지에 접근하려고 할 때, 막아주는 인증 middle
-// export default defineNuxtRouteMiddleware(() => {
-//     const member = useAuthMember(); // 로그인된 사용자 정보
-//
-//     // 인증이 필요한 경로
-//     // "/member/links/**" → 로그인한 사람만 접근 가능
-//     if (to.path.startsWith('/member/links')) {
-//         if (!member.value) {
-//             return navigateTo('/login');
-//         }
-//     }
-//
-//     // 본인만 접근 가능하게 제한
-//     // 예: /member/index.vue || bookmarks.vue  본인이 아닐 경우 차단
-//     if (to.path.startsWith('/member/links')) {
-//         const routeMemberId = member.value?.id; // 예: 본인의 ID
-//     }
-// });
+// 인증이 필요한 페이지에 접근하려고 할 때, 막아주는 인증 middle
+
+import useAuthMember from '~/composables/useMemberDetails'
+export default defineNuxtRouteMiddleware((to) => {
+
+    const { isAnonymous, memberId } = useMemberDetails()
+
+    // 인증이 필요한 경로
+    // "/member/links/**" → 로그인한 사람만 접근 가능
+    if (to.path.startsWith('/member/links')) {
+        if (isAnonymous()) {
+            return navigateTo('/login')
+        }
+    }
+
+    const CurrentMemberId = memberId.value // 로그인된 유저 ID
+    const routeMemberId = to.params.id     // URL 상의 :id
+
+    // 본인만 접근 가능하게 제한
+    // 예: /member/index.vue || bookmarks.vue  본인이 아닐 경우 차단
+    if (to.path.startsWith('/member/links')) {
+        if (routeMemberId && routeMemberId !== String(CurrentMemberId)) {
+            return navigateTo('/403') // 접근 차단 페이지
+        }
+    }
+});
