ea2be8885a48e4f2e7f5bebea2e8613b5298185b
jvn1o <junok0987@naver.com>
2025-06-28 14:30:26 +0900

    "/member/links" -> "/member/my/links" 로 디렉토리 구조 변경

--- frontend/devlinks/middleware/auth.ts
+++ frontend/devlinks/middleware/auth.ts
@@ -6,8 +6,8 @@ export default defineNuxtRouteMiddleware((to) => {
     const { isAnonymous, memberId } = useMemberDetails()
 
     // 인증이 필요한 경로
-    // "/member/links/**" → 로그인한 사람만 접근 가능
-    if (to.path.startsWith('/member/links')) {
+    // "/member/my/links/**" → 로그인한 사람만 접근 가능
+    if (to.path.startsWith('/member/my/links')) {
         if (isAnonymous()) {
             return navigateTo('/login')
         }
@@ -18,7 +18,7 @@ export default defineNuxtRouteMiddleware((to) => {
 
     // 본인만 접근 가능하게 제한
     // 예: /member/index.vue || bookmarks.vue  본인이 아닐 경우 차단
-    if (to.path.startsWith('/member/links')) {
+    if (to.path.startsWith('/member/my/links')) {
         if (routeMemberId && routeMemberId !== String(CurrentMemberId)) {
             return navigateTo('/403') // 접근 차단 페이지
         }
